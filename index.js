1dd5e3e7c07c6a3105219da0fa3c1cc7ea50bf75
Swati <swatic019@gmail.com>
2023-11-30 13:16:15 +0530

    dotenv added

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const weekList = mongoose.model("weekList", {
+  list: String,
+});
 
 app.get("/", (req, res) => {
   res.send("Express Server");
@@ -25,5 +32,10 @@ app.use((req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("server running suessfully!!");
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() =>
+      console.log(`Server running on http://localhost:${process.env.PORT}`)
+    )
+    .catch((err) => console.log(err));
 });
