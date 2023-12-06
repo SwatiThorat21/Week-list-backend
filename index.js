14bb941e353f44656f3ecfd8a62ed5b46f1e3e80
Swati <swatic019@gmail.com>
2023-12-06 16:26:51 +0530

    auth.js routes updated

--- index.js
+++ index.js
@@ -4,20 +4,15 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 const bcrypt = require("bcrypt");
+const authRoutes = require("./routes/auth")
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-
-app.get("/", (req, res) => {
-  res.json({
-    status: "SUCCESS",
-    message: "All good!",
-  });
 });
 
 app.get("/health", (req, res) => {
@@ -28,65 +23,14 @@ app.get("/health", (req, res) => {
   });
 });
 
-//Signup route
-app.post("/signup", async (req, res) => {
-  console.log(req.body);
-  try {
-    const { fullname, email, password, age, gender, mobile } = req.body;
-    const encryptedPassword = await bcrypt.hash(password, 10);
-    await User.create({
-      fullname,
-      email,
-      password: encryptedPassword,
-      age,
-      gender,
-      mobile,
-    });
-    res.json({
-      status: "SUCCESS",
-      message: "You've signed up sucessfully !!",
-    });
-  } catch (error) {
-    res.json({
-      status: "FAILED",
-      message: "Something went wrong !",
-    });
-  }
+app.get("/", (req, res) => {
+  res.json({
+    status: "SUCCESS",
+    message: "All good!",
+  });
 });
 
-//Login route
-app.post("/login", async (req, res) => {
-  // console.log(req.body)
-  try {
-    const { email, password } = req.body;
-    const user = User.findOne({ email });
-    // console.log(user)
-    if (user) {
-      let hasPasswordMatch = await bcrypt.compare(password, user.password);
-      if (hasPasswordMatch) {
-        res.json({
-          status: "SUCCESS",
-          message: "You've logged in sucessfully !!",
-        });
-      } else {
-        res.json({
-          status: "FAILED",
-          message: "incorrect credentials !",
-        });
-      }
-    } else {
-      res.json({
-        status: "FAILED",
-        message: "User does not exist !",
-      });
-    }
-  } catch (error) {
-    res.json({
-      status: "FAILED",
-      message: "incorrect credentials !",
-    });
-  }
-});
+app.use('/auth', authRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
--- routes/auth.js
+++ routes/auth.js
@@ -2,4 +2,73 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+
+//Signup route
+
+router.post("/register", async (req, res) => {
+  try {
+    const { fullname, email, password, age, gender, mobile } = req.body;
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    await User.create({
+      fullname,
+      email,
+      password: encryptedPassword,
+      age,
+      gender,
+      mobile,
+    });
+    res.json({
+      status: "SUCCESS",
+      message: "You've signed up sucessfully !!",
+    });
+  } catch (error) {
+    console.log(error)
+    res.json({
+      status: "FAILED",
+      message: "Something went wrong !",
+    });
+  }
+});
+
+//Login route
+
+router.post("/login", async (req, res) => {
+  // console.log(req.body)
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    // console.log(user)
+    if (user) {
+      let hasPasswordMatch = await bcrypt.compare(password, user.password);
+      if (hasPasswordMatch) {
+        const jwToken = jwt.sign(user.toJSON(), process.env.JWT_SCRETEKEY, {
+          expiresIn: 15,
+        });
+        res.json({
+          status: "SUCCESS",
+          message: "You've logged in sucessfully !!",
+          jwToken
+        });
+      } else {
+        res.json({
+          status: "FAILED",
+          message: "incorrect credentials !",
+        });
+      }
+    } else {
+      res.json({
+        status: "FAILED",
+        message: "User does not exist !",
+      });
+    }
+  } catch (error) {
+    console.log(error)
+    res.json({
+      status: "FAILED",
+      message: "incorrect credentials !",
+    });
+  }
+});
+
+module.exports = router;
